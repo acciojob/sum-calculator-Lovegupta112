f1864e1c6493529de66de7033ce972cbdd592c89
Love Gupta <90507983+Lovegupta112@users.noreply.github.com>
2023-07-23 17:40:23 +0000

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,29 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const[number,setNumber]=useState(0);
+  const[sum,setSum]=useState(0);
+
+  useEffect(()=>{
+   add();
+  },[number]);
+
+  function add(){
+
+    console.log(number);
+    setSum((sum)=>sum+number);
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        {/* <h1>Hello</h1> */}
+        <input type="number" onChange={(e)=>setNumber(parseInt(e.target.value))} value={number}/>
+         <p>Sum: {sum}</p>
     </div>
   )
 }
 
-export default App
+export default App;
